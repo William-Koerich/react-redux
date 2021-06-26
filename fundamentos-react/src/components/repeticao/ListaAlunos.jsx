7e2d3688b5af4e302d21bb05531da28a408348cb
William Koerich <williamkoerich@hotmail.com>
2021-06-26 18:18:43 -0300

    WIP

---
+++ fundamentos-react/src/components/repeticao/ListaAlunos.jsx
@@ -0,0 +1,16 @@
+import React from 'react'
+
+const studentList = props => {
+  return (
+    <div>
+      <ul>
+        <li>Ana - 9,7</li>
+        <li>João - 9,0</li>
+        <li>Mateus - 8,5</li>
+        <li>Felipe - 8,0</li>
+      </ul>
+    </div>
+  )
+}
+
+export default studentList
